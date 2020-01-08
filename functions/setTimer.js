a969f0ec743029d56983de6a9df32b923f0860f0
porinut <porinut1@gmail.com>
2020-01-09 03:17:32 +0700

    not

--- functions/setTimer.js
+++ functions/setTimer.js
@@ -12,14 +12,22 @@ module.exports = {
         var time = agent.parameters['time'];
         var date = agent.parameters['date'];
         var setTimer = agent.parameters['setTimer_entity'];
+
+        var timeUTC = moment(time).format('YYYY-MM-DDTHH:mm:ss-07:00');
+        var timeFormatted = moment.utc(timeUTC).format('HH:mm:ss');
+ 
+        var dateUTC = moment(date).format('YYYY-MM-DDTHH:mm:ss-07:00');
+        var dateFormatted = moment.utc(dateUTC).format('DD-MM-YYYY');
+
+
         try {
             console.log('number => '+number);
-            console.log('time => '+time);
-            console.log('date => '+date);
+            console.log('time => '+timeFormatted);
+            console.log('date => '+dateFormatted);
             console.log('setTimer_entity => '+setTimer);
             agent.add('number => '+number);
-            agent.add('time => '+time);
-            agent.add('date => '+date);
+            agent.add('time => '+timeFormatted);
+            agent.add('date => '+dateFormatted);
             agent.add('setTimer_entity => '+setTimer);
             console.log('------------------------------------------------------');
             console.log('Function handleSetTimer is run successfull');
